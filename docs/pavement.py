44881964b3970ea2fd84b089ad88313755dcce3d
Jody Garnett <jody.garnett@gmail.com>
2019-11-24 22:46:01 -0800

    working pavement for sphinx docs
    Signed-off-by: Jody Garnett <jody.garnett@gmail.com>

--- docs/pavement.py
+++ docs/pavement.py
@@ -2,33 +2,30 @@ import os
 from paver.easy import *
 import paver.doctools
 
-```
+'''
 Generate GeoServer Enterprise documentation.
-```
-
+'''
 options(
     setup=dict(
-    name='GeoServer Enterprise Documentation',
-    version="2.16.1",
-    description="Documentation for GeoServer Enterprise distribution by GeoCat.",
-    author="Jody Garnett",
-    author_email="jody.garnett@geocat.net",
-    
+        name='GeoServer Enterprise Documentation',
+        version='2.16.1',
+        description='Documentation for GeoServer Enterprise distribution by GeoCat.',
+        author='Jody Garnett',
+        author_email='jody.garnett@geocat.net',
+   ),
     sphinx=Bunch(
-       builddir="build/html",
-       sourcedir="src"
+       sourcedir='src',
+       docroot='.',
+       builder='html',
+       doctrees='build/doctrees',
+       builddir='build',
+       apidir = None
     )
-}
+)
 
 @task
 @needs('paver.doctools.html')
 def builddocs():
-    '''Create html docs and install into build/html'''
-    builddocs = path()
-    cwd = os.getcwd()
-    tmpDir = os.path.join(cwd, 'tmp')    
-    for product in products:
-        productFolder = os.path.join(tmpDir, product)        
-        refs = getrefs(productFolder)
-        for refname, ref in refs.items():
-            build_product_doc(productFolder, refname, ref)
+    '''
+    Create html docs and install into build/html
+    '''
--- docs/src/conf.py
+++ docs/src/conf.py
@@ -42,7 +42,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['../templates']
+templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -74,7 +74,10 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'geocat'
+
+# Add any paths that contain custom themes here, relative to this directory.
+html_theme_path = ['../themes']
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
