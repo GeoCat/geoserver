04de6bc6b03de5a86bccd243b404be4d6d4a916e
Michel Gabriël <michel.gabriel@geocat.net>
2019-12-11 15:23:40 +0100

    Added example and new config options

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -100,7 +100,19 @@ html_theme_path = ['../themes']
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False
+}
+
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = '../themes/' + html_theme + '/static/img/gc_geocat_logo_text_500.png'
+
+# The name of an image file (within the static path) to use as favicon of the
+# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+html_favicon = 'favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
