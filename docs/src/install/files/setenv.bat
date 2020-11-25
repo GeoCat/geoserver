1a7f1639ecca8eb1dec5d0b49c834af6ab94d3df
Jody Garnett <jody.garnett@geocat.net>
2020-11-25 20:18:40 +0100

    prepend jvm options, append system properties

--- docs/src/install/files/setenv.bat
+++ docs/src/install/files/setenv.bat
@@ -1,1 +1,5 @@
-SET CATALINA_OPTS="%CATALINA_OPTS% -XX:SoftRefLRUPolicyMSPerMB=36000 -XX:-UsePerfData -Dorg.geotools.referencing.forceXY=true"
+Rem Prepend JVM Options
+SET CATALINA_OPTS="-XX:SoftRefLRUPolicyMSPerMB=36000 -XX:-UsePerfData %CATALINA_OPTS%"
+
+Rem Append system property values
+SET CATALINA_OPTS="%CATALINA_OPTS% -Dorg.geotools.referencing.forceXY=true""
