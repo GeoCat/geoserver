03b05ca190089f23376ad6302993e96edfb44b9d
Jody Garnett <jody.garnett@geocat.net>
2020-11-25 20:24:57 +0100

    Add Marlin respecting jvm argument order

--- docs/src/install/files/setenv_marlin.sh
+++ docs/src/install/files/setenv_marlin.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-CATALINA_OPTS="${CATALINA_OPTS} -XX:SoftRefLRUPolicyMSPerMB=36000 -XX:-UsePerfData -Xbootclasspath/a=lib/marlin-0.9.4.3-Unsafe.jar -Dorg.geotools.referencing.forceXY=true -Dsun.java2d.renderer=org.marlin.pisces.MarlinRenderingEngine"
+
+# Prepend JVM Options
+CATALINA_OPTS="-XX:SoftRefLRUPolicyMSPerMB=36000 -XX:-UsePerfData ${CATALINA_OPTS}"
+
+# Append system properties
+CATALINA_OPTS="${CATALINA_OPTS} -Dorg.geotools.referencing.forceXY=true"
+
+# Java 8: Marlin
+CATALINA_OPTS="-Xbootclasspath/a=lib/marlin-0.9.4.3-Unsafe.jar ${CATALINA_OPTS} -Dsun.java2d.renderer=org.marlin.pisces.MarlinRenderingEngine"
